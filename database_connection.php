b726b7a777ea6d7634740b0ab58589365b2badec
Vivek Hanasoge <46605922+vivekh99@users.noreply.github.com>
2019-09-22 23:14:50 -0400

    updated to connect and query from new database

--- database_connection.php
+++ database_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbName = "courses_except_engr";
+$dbName = "f19-um-courses";
 
 // Create connection
 $myDB = new mysqli($servername, $username, $password, $dbName);
--- search_text.php
+++ search_text.php
@@ -5,14 +5,20 @@ include "database_connection.php";
 //search database for input
 
 $text_from_search = $_GET["search"];
-$sql = "SELECT `Class Name`, `A+` FROM myTable WHERE `A+` >= '$text_from_search' ";
+//$text_from_dropdown_workload = $_GET["workload"];
+//$text_from_dropdown_grades = $_POST["grades"];
+//$text_from_search = preg_replace("#[^1-100]#", "", $text_from_search);
+
+$sql = "SELECT ClassName, `A+`, 'Workload' FROM courses WHERE `A+` >= '$text_from_search'";
+//$sql = "SELECT 'Workload' FROM myTable WHERE 'Workload' <= '$text_from_dropdown_workload' ";
+
 $result = $myDB->query($sql);
 
 if ($result->num_rows > 0) {
 
 	//output data of row
 	while ($row = $result->fetch_assoc()) {
-		echo $row["Class Name"] . " A+ " . $row["A+"] . "<br>";
+		echo $row["ClassName"] . " A+ " . $row["A+"] . "<br>";
 	}
 }
 else {
